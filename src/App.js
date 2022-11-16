17d7a1397702c2ff7f92d55608781caecfa3c1c4
MunFung <leemunfung03@gmail.com>
2022-11-16 11:49:46 +0800

    Modify

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const expense = [
     { id: 'e1', title: 'Car Insurance', amount: 123, date: new Date(2021, 12, 28) },
     { id: 'e2', title: 'Toilet Paper', amount: 453, date: new Date(2021, 3, 21) },
-    { id: 'e3', title: 'Toothbrush', amount: 783, date: new Date(2021, 4, 20) },
+    { id: 'e3', title: 'Toothbrush', amount: 333, date: new Date(2021, 4, 20) },
   ];
   
     /* <div className="App">
