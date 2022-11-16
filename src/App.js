27b353aeb7afa6f2695c90368c26a7cde425b4f0
kelvinlam2101 <hylam98tom@gmail.com>
2022-11-16 11:40:09 +0800

    transferingFiles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './App.css';
 //import ExpenseItem from './components/ExpenseItem';
-import Expense from './components/Expense';
+import Expense from './components/Expenses/Expense';
 
 function App() {
   /*const word = document.createElement('p');
