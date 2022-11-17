16c35535ad2fcda957e9a89d39d73b71672c63b6
MunFung <leemunfung03@gmail.com>
2022-11-17 22:58:31 +0800

    outputtingConditionalContent

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -5,31 +5,52 @@ import ExpenseFilter from './ExpensesFilter.js';
 import React, { useState } from 'react';
 
 function Expense(props) {
-    /*const expense = [
-        { id: 'e1', title: 'Car Insurance', amount: 123, date: new Date(2021, 12, 28) },
-        { id: 'e2', title: 'Toilet Paper', amount: 453, date: new Date(2021, 3, 21) },
-        { id: 'e3', title: 'Toothbrush', amount: 783, date: new Date(2021, 4, 20) },
-      ];
-      */
-     const [filterYear, setFilterYear] = useState('2020');
-      const filterHandler = (selectedYear) => {
-        setFilterYear(selectedYear);
-      };
-    return (
-      <div>
-        <Card className="expenses">
-        <ExpenseFilter selected={filterYear} onSaveSelectedYear={filterHandler}></ExpenseFilter>
-      {props.items.map((expense) => (
-      <ExpenseItem 
-        title={expense.title} 
-        amount={expense.amount} 
+  /*const expense = [
+      { id: 'e1', title: 'Car Insurance', amount: 123, date: new Date(2021, 12, 28) },
+      { id: 'e2', title: 'Toilet Paper', amount: 453, date: new Date(2021, 3, 21) },
+      { id: 'e3', title: 'Toothbrush', amount: 783, date: new Date(2021, 4, 20) },
+    ];
+    */
+  const [filterYear, setFilterYear] = useState('2020');
+  const filterHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+  const filterExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
+  let expensesContent = <p>No expense found</p>;
+  if(filterExpenses.length > 0){
+    expensesContent = filterExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
         date={expense.date}>
 
       </ExpenseItem>
-      ))}
-        </Card>
-</div>
-    );
+    ))
+  }
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter selected={filterYear} onSaveSelectedYear={filterHandler}></ExpenseFilter>
+        {expensesContent}
+      </Card>
+    </div>
+  );
 }
+/*  for study purpose
+        {filterExpenses.length === 0 && <p>No expense found</p>}
+        {filterExpenses.length > 0 &&
+          filterExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}>
 
+            </ExpenseItem>
+          ))}
+*/
 export default Expense;
